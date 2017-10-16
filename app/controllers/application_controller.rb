36d0fbc148c3c99f2eab9d623ceb9c2cd81e2a75
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-16 14:36:59 -0700

    require user is logged in for all pages except root (and login)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  before_action :require_login
 
   def render_404
     # DPR: supposedly this will actually render a 404 page in production
     raise ActionController::RoutingError.new('Not Found')
   end
 
-private
+  protected
+  def require_login
+    if session[:user_id].nil?
+      flash[:status] = :failure
+      flash[:result_text] = 'You must be logged in to do that.'
+      return redirect_to root_path
+    end
+  end
+
+  private
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])

aed0eba8d64ea3bf3fa76439a2d02c68a562d966
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-16 12:33:50 -0700

    update users schema to take email, UID, provider, name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,27 +5,26 @@ class SessionsController < ApplicationController
   def login
     auth_hash = request.env['omniauth.auth']
 
-    raise
-    # username = params[:username]
-    # if username and user = User.find_by(username: username)
-    #   session[:user_id] = user.id
-    #   flash[:status] = :success
-    #   flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    # else
-    #   user = User.new(username: username)
-    #   if user.save
-    #     session[:user_id] = user.id
-    #     flash[:status] = :success
-    #     flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-    #   else
-    #     flash.now[:status] = :failure
-    #     flash.now[:result_text] = "Could not log in"
-    #     flash.now[:messages] = user.errors.messages
-    #     render "login_form", status: :bad_request
-    #     return
-    #   end
-    # end
-    # redirect_to root_path
+    user = User.find_by(uid: auth_hash['uid'])
+
+    if user
+      session[:user_id] = user.id
+      flash[:status] = :success
+      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+    else
+      user = User.by_auth_hash(provider, auth_hash)
+      if user.save
+        flash[:status] = :success
+        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+      else
+        flash.now[:status] = :failure
+        flash.now[:result_text] = "Could not log in"
+        flash.now[:messages] = user.errors.messages
+        return render "login_form", status: :bad_request
+      end
+      session[:user_id] = user.id
+      redirect_to root_path
+    end
   end
 
   def logout

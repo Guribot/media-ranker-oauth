2f12fbaeb11be92641015c0bd2b033808ad8f769
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-16 12:19:14 -0700

    more omniauth setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,26 +3,29 @@ class SessionsController < ApplicationController
   end
 
   def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-      else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
-      end
-    end
-    redirect_to root_path
+    auth_hash = request.env['omniauth.auth']
+
+    raise
+    # username = params[:username]
+    # if username and user = User.find_by(username: username)
+    #   session[:user_id] = user.id
+    #   flash[:status] = :success
+    #   flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+    # else
+    #   user = User.new(username: username)
+    #   if user.save
+    #     session[:user_id] = user.id
+    #     flash[:status] = :success
+    #     flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+    #   else
+    #     flash.now[:status] = :failure
+    #     flash.now[:result_text] = "Could not log in"
+    #     flash.now[:messages] = user.errors.messages
+    #     render "login_form", status: :bad_request
+    #     return
+    #   end
+    # end
+    # redirect_to root_path
   end
 
   def logout
